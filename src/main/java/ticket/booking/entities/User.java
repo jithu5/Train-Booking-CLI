0db1527ff6fb5bb56243491e555e6056adf59a02
jithu5 <abijithr202@gmail.com>
2025-05-22 14:31:00 +0530

    initial commit

---
+++ src/main/java/ticket/booking/entities/User.java
@@ -0,0 +1,15 @@
+package ticket.booking.entities;
+
+import java.util.List;
+
+public class User {
+    private String name;
+    private  String password;
+    private  String hashPassword;
+    private List<Ticket> ticketsBooked;
+    private  String userId;
+    public static void main(String[] args) {
+
+
+    }
+}
